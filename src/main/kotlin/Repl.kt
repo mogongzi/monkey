d08f87de2303d96daaba00207e26ed8fb6f7a76e
mogongzi <superrenyuan@hotmail.com>
2026-02-18 21:41:38 +0800

    finish Read-Parse-Print-Loop

--- src/main/kotlin/Repl.kt
+++ src/main/kotlin/Repl.kt
@@ -1,12 +1,14 @@
 package me.ryan.interpreter
 
 import me.ryan.interpreter.lexer.Lexer
+import me.ryan.interpreter.parser.Parser
 import me.ryan.interpreter.token.EOF
 
 private const val PROMPT = ">> "
 
-fun main() {
-    println("🐒")
+fun main(args: Array<String>) {
+    val lexerMode = args.contains("--lexer")
+    if (lexerMode) println("🐒 Lexer") else println("🐵 Parser")
     while (true) {
         print(PROMPT)
         System.out.flush()
@@ -14,10 +16,29 @@ fun main() {
         val line = readlnOrNull() ?: return
 
         val lexer = Lexer(line)
-        var tok = lexer.nextToken()
-        while (tok.type != EOF) {
-            println(tok)
-            tok = lexer.nextToken()
+
+        if (lexerMode) {
+            var tok = lexer.nextToken()
+            while (tok.type != EOF) {
+                println(tok)
+                tok = lexer.nextToken()
+            }
+        } else {
+            val parser = Parser(lexer)
+            val program = parser.parseProgram()
+            if (parser.errors().isNotEmpty()) {
+                printParserErrors(parser.errors())
+                continue
+            }
+            println(program.string())
         }
+    }
+}
+
+private fun printParserErrors(errors: List<String>) {
+    println("❌ Woops! We ran into some monkey business here!")
+    println(" Parser errors:")
+    for (msg in errors) {
+        println("\t$msg")
     }
 }
